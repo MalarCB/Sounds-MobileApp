faf2d4f049c23c00554e31844644638b2fef9bf5
MalarCB <malarvizhy.soupramanien@careerbuilder.com>
2015-07-24 11:36:20 +0200

    Initial Commit

---
+++ Sounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sounds
+//
+//  Created by Malarvizhy Soupramanien on 7/24/15.
+//  Copyright © 2015 CB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
